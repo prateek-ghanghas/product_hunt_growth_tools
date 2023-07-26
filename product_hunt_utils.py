cb42ed0d7c673db93311ae17207bce6a6c59a017
prateek-ghanghas <139649603+prateek-ghanghas@users.noreply.github.com>
2023-07-27 00:49:15 +0530

    Update product_hunt_utils.py

--- product_hunt_utils.py
+++ product_hunt_utils.py
@@ -2,59 +2,31 @@ import requests
 from bs4 import BeautifulSoup as bs
 import json
 
-URL = "https://www.producthunt.com/"
+def get_commentor_info_for_product_id(product_id):
 
-response = requests.get(URL)
-
-html = bs(response.content, 'lxml')
-
-
-tags = html.find('script', attrs = {'id':"__NEXT_DATA__"}).string.strip()
-
-
-data = json.loads(tags)
-
-
-data = data["props"]["apolloState"]
-
-refrences = []  #refrences will have product ids (not as it is)
-
-for dict_ in data:
-        if "HomefeedPageFEATURED" in dict_:
-            for count in range(len(data[dict_]["items"])):
-                ref.append(data[dict_]["items"][count]["__ref"])
-
-upvote_list = []
-
-for ref in refrences:
-      if "Ad" in k or "Discussion" in k or "Antholo" in k or "Collection" in k:
-            continue
-      json_data = {
-            'operationName': 'PostPageSocialProof',
-            'variables': {
-            'postId': k[4:],
-            'limit': 500,
+        '''
+        This function fetches commenters and upvoters names by providing the id of that paricular product
+        :param product_id: sample: 402396
+        :return list: sample: [....]
+        '''
+        upvote_and_commenters_list = []
+        
+        json_data = {
+              'operationName': 'PostPageSocialProof',
+              'variables': {
+              'postId': product_id,
+              'limit': 1000,
                          },
-            'query': 'query PostPageSocialProof($postId:ID!$limit:Int!){post(id:$postId){id contributors(limit:$limit){role user{id ...UserImage __typename}__typename}__typename}}fragment UserImage on User{id name username avatarUrl __typename}',
+              'query': 'query PostPageSocialProof($postId:ID!$limit:Int!){post(id:$postId){id contributors(limit:$limit){role user{id ...UserImage __typename}__typename}__typename}}fragment UserImage on User{id name username avatarUrl __typename}',
                  }
-      res = requests.post('https://www.producthunt.com/frontend/graphql', json=json_data)
-      res = res.json()
-      upvoters_data = res["data"]["post"]["contributors"]
-      names_list = []
-      upvote_dic = {}
-      upvote_maindic = {}
-      for dict in upvoters_data:
-            if dict["role"] == "upvoter":
-                  names_list.append(dict["user"]["name"])
-      upvote_dic["product_id"] = int(k[4:])
-      upvote_dic["upvoter_names"] = l
-      upvote_maindic.update(upvote_dic)
-      upvote_list.append(upvote_maindic)
-
-
-
-product_id = int(input("enter product id: "))
-
-for dictionary in upvote_list:
-      if dictionary["product_id"] == product_id:
-          print(dictionary["upvoter_names"])
+        res = requests.post('https://www.producthunt.com/frontend/graphql', json=json_data)
+        res = res.json()
+        upvoters_data = res["data"]["post"]["contributors"]
+        for dict in upvoters_data:
+              if dict["role"] == "upvoter" or dict["role] == "commenter":
+                      upvote_and_commenters_list.append(dict["user"]["name"])
+        print(upvote_and_commenters_list)
+if __name__ == "__main__":
+        product_id = int(input("Enter the product id: "))
+        get_commentor_info_for_product_id(product_id)
+        
