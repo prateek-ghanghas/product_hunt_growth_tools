88fe94e732417e558605080776d96f09920d6f19
prateek-ghanghas <139649603+prateek-ghanghas@users.noreply.github.com>
2023-07-27 12:08:05 +0530

    Update product_hunt_utils.py
    1. added URL as global variable
    2. sample provided for what this function return

--- product_hunt_utils.py
+++ product_hunt_utils.py
@@ -2,12 +2,14 @@ import requests
 from bs4 import BeautifulSoup as bs
 import json
 
+PRODUCT_HUNT_GRAPHQL_API = 'https://www.producthunt.com/frontend/graphql'
+
 def get_commentor_info_for_product_id(product_id):
 
         '''
         This function fetches commenters and upvoters names by providing the id of that paricular product
         :param product_id: sample: 402396
-        :return list: sample: [....]
+        :return upvote_and_commenters_list: sample: ['Nick Anisimov', 'Cyril Gupta', 'Victor Antonov', 'Shoaib Iqbal',........]
         '''
         upvote_and_commenters_list = []
         
@@ -19,7 +21,7 @@ def get_commentor_info_for_product_id(product_id):
                          },
               'query': 'query PostPageSocialProof($postId:ID!$limit:Int!){post(id:$postId){id contributors(limit:$limit){role user{id ...UserImage __typename}__typename}__typename}}fragment UserImage on User{id name username avatarUrl __typename}',
                  }
-        res = requests.post('https://www.producthunt.com/frontend/graphql', json=json_data)
+        res = requests.post(PRODUCT_HUNT_GRAPHQL_API, json=json_data)
         res = res.json()
         upvoters_data = res["data"]["post"]["contributors"]
         for dict in upvoters_data:
